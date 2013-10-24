cdf2c96656ea16a7a3905bc763c7273c74742e40
Vladimir Magyar <magyarvladimir@gmail.com>
2013-10-24 16:01:17 +0200

    testovanie

--- nntest.c
+++ nntest.c
@@ -3,11 +3,11 @@
 int main(int argc, char** argv)
 {
     printf("start\n");
-    micronn* net = micronn_init(4, 3, 1, 13);
+    micronn* net = micronn_init(4, 3, 1, 5);
     micronn_matrix* i = micronn_matrix_read(fopen("input.data", "r"));
     micronn_matrix* o = micronn_matrix_read(fopen("targets.data", "r"));
 
-    micronn_train(net, i, o, 0.1, 0.1, 0, 2.3, 1000);
+    micronn_train(net, i, o, 0.05, 0.1, 0, 2.3, 1000);
     micronn_matrix_free(i);
     i = micronn_matrix_read(fopen("input.data", "r"));
     micronn_matrix* m = micronn_forward(net, i);
